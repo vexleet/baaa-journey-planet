d3fd23bdbc61a4d416ab565812b59642eaf39afe
vexleet <abda70226@gmail.com>
2022-10-26 17:27:55 +0200

    add class name for individual trips and added constant values for tabs

--- src/components/Tabs/Tab.jsx
+++ src/components/Tabs/Tab.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 
-const Tab = ({ label }) => {
-  return <p>{label}</p>;
+const Tab = ({ label, className }) => {
+  return <p className={className}>{label}</p>;
 };
 
 Tab.propTypes = {
   label: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired
+  value: PropTypes.string.isRequired,
+  className: PropTypes.string
 };
 
 export default Tab;
--- src/pages/MyProfile/MyProfile.jsx
+++ src/pages/MyProfile/MyProfile.jsx
@@ -2,18 +2,24 @@ import Tabs from '@/components/Tabs/index.jsx';
 import { useState } from 'react';
 
 const MyProfile = () => {
-  const [selectedTab, setSelectedTab] = useState('');
+  const tabValues = {
+    trips: 'trips',
+    pingboards: 'pingboards',
+    pings: 'pings'
+  };
+
+  const [selectedTab, setSelectedTab] = useState(tabValues.trips);
 
   return (
     <div>
       <Tabs onChange={setSelectedTab} value={selectedTab}>
-        <Tabs.Tab value="test333" label="test333"></Tabs.Tab>
-        <Tabs.Tab value="test21" label="test21"></Tabs.Tab>
-        <Tabs.Tab value="test123" label="test123"></Tabs.Tab>
+        <Tabs.Tab value={tabValues.trips} label="Trips"></Tabs.Tab>
+        <Tabs.Tab value={tabValues.pingboards} label="Pingboard"></Tabs.Tab>
+        <Tabs.Tab value={tabValues.pings} label="Pings"></Tabs.Tab>
 
-        <Tabs.TabPanel value="test333">hello world</Tabs.TabPanel>
-        <Tabs.TabPanel value="test21">hello world1</Tabs.TabPanel>
-        <Tabs.TabPanel value="test123">hello world2</Tabs.TabPanel>
+        <Tabs.TabPanel value={tabValues.trips}>hello world</Tabs.TabPanel>
+        <Tabs.TabPanel value={tabValues.pingboards}>hello world1</Tabs.TabPanel>
+        <Tabs.TabPanel value={tabValues.pings}>hello world2</Tabs.TabPanel>
       </Tabs>
     </div>
   );
