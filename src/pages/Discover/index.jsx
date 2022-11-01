60e0921556112ad8c05ef363a1385a5aa658a49f
hirannis13 <keczodori2@gmail.com>
2022-11-01 16:51:59 +0100

    Design of coming soon page with discover - people

--- src/pages/Discover/index.jsx
+++ src/pages/Discover/index.jsx
@@ -72,16 +72,22 @@ const Discover = () => {
       </div>
 
       {displayPar === displayTogglerItems[1] && (
-        <div>
-          <h2>COMING SOON!</h2>
+        <div className="comingsoon-wrapper">
+          <div className="comingsoon-header">
+            <img src="../src/assets/icons/confettileft.svg" alt="Confetti Celebration Left" />
+            <h2>COMING SOON!</h2>
+            <img src="../src/assets/icons/confettiright.svg" alt="Confetti Celebration Right" />
+          </div>
           <p>If you want to be up-to-date with new features coming, get notified!</p>
           <TextField
             id="notify"
             className="textfield-emailNotify"
             placeholder={'email@mail.com'}
-            type="email"
+            type="text"
           />
-          <button>Notify</button>
+          <div className="notifyme-button">
+            <button>Notify</button>
+          </div>
         </div>
       )}
 
